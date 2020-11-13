87da95ba682b5f9152b6f44437152a0b4e41bf7f
Opilane TTHK <opilane@tthk.ee>
2020-11-13 09:10:08 +0200

    first commit

--- fryday/Program.cs
+++ fryday/Program.cs
@@ -8,25 +8,17 @@ namespace fryday
         static void Main(string[] args)
         {
 
-            string samplePath = @"C:\Users\opilane\samples";
+            string rootPath = @"C:\Users\opilane\samples";
             string directoriesTxt = @"C:\Users\opilane\Friday\sampleDirectories.txt";
             string filesTxt = @"C:\Users\opilane\Friday\sampleFiles.txt";
 
-            DirectoryWriter(directoriesTxt, samplePath);
-            FileWriter(filesTxt, samplePath);
-            Console.WriteLine("Written!!");
+            string[] includeNestedDirecotries = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+            string[] allFiles = Directory.GetFiles(rootPath, "*", SearchOption.AllDirectories);
 
-        }
-        public static void DirectoryWriter(string fileName, string filePath)
-        {
-            string[] getNested = Directory.GetDirectories(filePath, "*", SearchOption.AllDirectories);
-            File.WriteAllLines(fileName, getNested);
-        }
-        public static void FileWriter(string fileName, string filePath)
-        {
-            string[] allFiles = Directory.GetFiles(filePath, "*", SearchOption.AllDirectories);
-            File.WriteAllLines(fileName, allFiles);
+            File.WriteAllLines(directoriesTxt, includeNestedDirecotries);
+            File.WriteAllLines(filesTxt, allFiles);
+            Console.WriteLine("Complete");
         }
     }
-    }
-    
+}
+
